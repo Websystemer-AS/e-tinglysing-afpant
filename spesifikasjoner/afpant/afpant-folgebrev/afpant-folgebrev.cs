89503104f732d0b5658dd989d6cde7be9c924976
Pål Havnevik <paal@websystemer.as>
2017-03-14 10:39:46 +0100

    initial request for comments (c# class will be exported to XSD after final review)

---
+++ spesifikasjoner/afpant/afpant-folgebrev/afpant-folgebrev.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFPANT
+{
+    public class Folgebrev
+    {
+        public List<RettighetsHaver> Kreditorer { get; set; }
+
+        public List<RettighetsHaver> Debitorer { get; set; }
+
+        public List<MatrikkelEnhet> MatrikkelEnheter { get; set; }
+
+        public Pantedokument PantedokumentDetaljer { get; set; }
+
+        public Overfoersel OverfoerselDetaljer { get; set; }
+
+        public List<String> Forutsetninger {get;set; }
+
+        public string AnneFritekst { get; set; }
+
+        public Adresse ReturAdresse { get; set; }
+
+        public class Overfoersel
+        {
+            public double Beloep { get; set; }
+            public string TilKontonummer { get; set; }
+            public string KID { get; set; }
+            public string KreditorSaksnummer { get; set; }
+            public string MeglerSaksnummer { get; set; }
+            public string BeloepGjelder { get; set; }
+        }
+
+        public class MatrikkelEnhet
+        {
+            public string Kommunenummer { get; set; }
+            public string Gardsnummer { get; set; }
+            public string Bruksnummer { get; set; }
+            public string Festenummer { get; set; }
+            public string Seksjonsnummer { get; set; }
+            public string Organisasjonsnummer { get; set; }
+            public string Andelsnummer { get; set; }
+        }
+
+        public class RettighetsHaver
+        {
+            public string Nummer { get; set; }
+            public string Navn { get; set; }
+        }
+
+        public class Adresse
+        {
+            public string Navn { get; set; }
+            public string Postnummer { get; set; }
+            public string Poststed { get; set; }
+        }
+
+        public class Pantedokument
+        {
+            public double Beloep { get; set; }
+            public int Prioritet { get; set; }
+        }
+    }
+}
